d73febbf2838d850b08b2539f068f3c3b882352b
Chandra Holt <chandra_holt@hotmail.com>
2022-10-17 18:16:42 -0700

    Formatted code

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,91 @@
 //Packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const markdown = require('./utilities/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdown = require("./utilities/generateMarkdown");
 
-//Questions for user input 
+//Questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: "What is the title of your project?",
-        name: 'title',
-        default: 'Project Title',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid project title is required.");
-            }
-            return true;
-        }
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+    default: "Project Title",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("A valid project title is required.");
+      }
+      return true;
     },
-    {
-        type: 'input',
-        message: "Please describe your project.",
-        name: 'description',
-        default: 'Project Description',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A  project description is required.");
-            }
-            return true;
-        }
+  },
+  {
+    type: "input",
+    message: "Please describe your project.",
+    name: "description",
+    default: "Project Description",
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("A  project description is required.");
+      }
+      return true;
     },
-    {
-        type: 'input',
-        message: "Describe the steps required for installation.",
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: "Please describe the usage of your project.",
-        name: 'usage',
-        default: 'Project Usage',
-    },
-    {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'none'],
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: "Please describe any relevant contributors, or how users can contribute to your project.",
-        name: 'contributing'
-    },
-    {
-        type: 'input',
-        message: "If applicable, provide any tests written for your application.",
-        name: 'tests'
-    },
- 
-    {
-        type: 'input',
-        message: "What is your GitHub username? (No @ needed)",
-        name: 'github',
-        default: 'chandrapanda',
-    },
-    {
-        type: 'input',
-        message: "What is your email address?",
-        name: 'email',
-        default: 'chandra_holt@hotmail.com',
-    }
-];
+  },
+  {
+    type: "input",
+    message: "Describe the steps required for installation.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please describe the usage of your project.",
+    name: "usage",
+    default: "Project Usage",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your project.",
+    choices: [
+      "Mozilla Public License 2.0",
+      "Apache License 2.0",
+      "MIT License",
+      "Boost Software License 1.0",
+      "none",
+    ],
+    name: "license",
+  },
+  {
+    type: "input",
+    message:
+      "Please describe any relevant contributors, or how users can contribute to your project.",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "If applicable, provide any tests written for your application.",
+    name: "tests",
+  },
 
+  {
+    type: "input",
+    message: "What is your GitHub username? (No @ needed)",
+    name: "github",
+    default: "chandrapanda",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    default: "chandra_holt@hotmail.com",
+  },
+];
 
 //Function to write README file
 function writeReadMe() {
-    inquirer
-        .prompt(questions)
-        .then((responses) => {
-            console.log(responses);
-            const myMarkdown = markdown(responses.license);
-            fs.writeFile('newREADME.md', `# ${responses.title}
+  inquirer.prompt(questions).then((responses) => {
+    console.log(responses);
+    const myMarkdown = markdown(responses.license);
+    fs.writeFile(
+      "newREADME.md",
+      `# ${responses.title}
 
 ## Description
 ${responses.description}
@@ -103,14 +109,14 @@ ${responses.tests}
 [GitHub](https://www.github.com/${responses.github}) 
 ### Email
 ${responses.email}
-${myMarkdown}`, function(err) {
-    if (err) throw err;
-    console.log('README created!');
-    
-})
-})
-};
+${myMarkdown}`,
+      function (err) {
+        if (err) throw err;
+        console.log("README created!");
+      }
+    );
+  });
+}
 
 // Function call to initialize app
 writeReadMe();
-
--- assets/utilities/generateMarkdown.js
+++ assets/utilities/generateMarkdown.js
@@ -2,14 +2,14 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(selectedLicense) {
   console.log(selectedLicense);
-  if (selectedLicense === 'Mozilla Public License 2.0') {
+  if (selectedLicense === "Mozilla Public License 2.0") {
     return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
-  } else if (selectedLicense === 'Apache License 2.0') {
+  } else if (selectedLicense === "Apache License 2.0") {
     return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  } else if (selectedLicense === 'MIT License') {
+  } else if (selectedLicense === "MIT License") {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  } else if (selectedLicense === 'Boost Software License 1.0') {
-    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if (selectedLicense === "Boost Software License 1.0") {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
   } else {
     return ``;
   }
@@ -18,13 +18,13 @@ function renderLicenseBadge(selectedLicense) {
 // Returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(selectedLicense) {
-  if (selectedLicense === 'Mozilla Public License 2.0') {
+  if (selectedLicense === "Mozilla Public License 2.0") {
     return `[This application is licensed under Mozilla](https://opensource.org/licenses/MPL-2.0)`;
-  } else if (selectedLicense === 'Apache License 2.0') {
+  } else if (selectedLicense === "Apache License 2.0") {
     return `[This application is licensed under Apache]((https://opensource.org/licenses/Apache-2.0))`;
-  } else if (selectedLicense === 'MIT License') {
+  } else if (selectedLicense === "MIT License") {
     return `[This application is licensed under MIT](https://opensource.org/licenses/MIT)`;
-  } else if (selectedLicense === 'Boost Software License 1.0') {
+  } else if (selectedLicense === "Boost Software License 1.0") {
     return `[This application is licensed under Boost](https://www.boost.org/LICENSE_1_0.txt)`;
   } else {
     return ``;
@@ -34,9 +34,9 @@ function renderLicenseLink(selectedLicense) {
 // Returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(selectedLicense) {
-  if (selectedLicense == 'none') {
+  if (selectedLicense == "none") {
     return ``;
-  } else { 
+  } else {
     return `## License `;
   }
 }
